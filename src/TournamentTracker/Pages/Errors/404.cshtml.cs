960557fb4582bc0ad598daaf028e7605370e1abb
N1K0232 <ns.nicolasilvestri@libero.it>
2025-12-10 13:48:32 +0100

    Add error pages

---
+++ src/TournamentTracker/Pages/Errors/404.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TournamentTracker.Pages.Errors;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class _404Model : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ src/TournamentTracker/Pages/Errors/500.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TournamentTracker.Pages.Errors;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class _500Model : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
